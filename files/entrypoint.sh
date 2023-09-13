a983a72abed0503d6a0e36c97bdd5e861a736421
msihoudini <144592021+msihoudini@users.noreply.github.com>
2023-09-13 03:38:08 +0000

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'49cd0f75-ac94-4a70-8dc3-801bebc2af45'}
 WEB_USERNAME=${WEB_USERNAME:-'admin'}
 WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 
